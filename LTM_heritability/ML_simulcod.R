f50c45350d72b93376933379154df8c68f70ae5c
WONJI\Wonji Kim <dnjswlzz11@gmail.com>
2018-11-12 03:55:51 -0500

    [LTMH] 1. paper

--- LTM_heritability/ML_simulcod.R
+++ LTM_heritability/ML_simulcod.R
@@ -319,27 +319,37 @@ est.ft(0.2,0.2,500,24)
 est.ft(0.2,0.4,500,20)
 
 ## n14, 32 cores, p 0.05 h2 0.05,0.2,0.4
-#### Convergence of heritability
+
+for(h2 in c(0.05,0.2,0.4)) est.ft(0.05,h2,500,32)
+
+
+
+
+#### new Convergence of heritability
 est.ft <- function(PREV,H2,N.fam,n.cores){
   source('~/paper/heritability/ML_ver2/LTM_heritability_ML_ver2.R')
   for(prev in PREV){
-    for(h2 in H2){
-      for(totalfam in N.fam){
-        print(paste0('prevalence:',prev,', heritability:',h2,', number of families:',totalfam))
-        library(kinship2)
-        setwd("~/paper/heritability/ML_ver2/variousFam/")
-        fin.dat <- read.table(paste0("prev_",prev,"_h2_",h2,"/dataset_add.txt"),head=T,stringsAsFactor=F)
-        fin.dat$std_snp <- (fin.dat$snp-mean(fin.dat$snp))/sd(fin.dat$snp)
-        fin.dat <- fin.dat[fin.dat$FID%in%fin.dat$FID[fin.dat$ind==1 & fin.dat$Y==1],,drop=F]
-        PB <- 'ind'
-        
-        model <- Y~std_snp-1
-        out <- paste0("~/paper/heritability/ML_ver2/variousFam/prev_",prev,"_h2_",h2,"/Esth2_",totalfam,"_asc.txt")
-        write.table(data.frame('obs','beta_std_snp','h2','n_iteration'),out,col.names=F,row.names=F,quote=F)
-        Esth2 <- sapply(1:300,getEsth2.asc,fin.dat=fin.dat,init_beta=0.1,init_h2=h2,totalfam=totalfam,assumed_prev=prev,model=model,n.cores=n.cores,out=out,seed=T,PB=PB)
-      }
-    }
+  	for(h2 in H2){
+  		for(totalfam in N.fam){
+  			print(paste0('prevalence:',prev,', heritability:',h2,', number of families:',totalfam))
+  			library(kinship2)
+  			setwd("~/paper/heritability/ML_ver2/variousFam/")
+  			fin.dat <- read.table(paste0("prev_",prev,"_h2_",h2,"/dataset.txt"),head=T,stringsAsFactor=F)
+  			fin.dat$std_snp <- (fin.dat$snp-mean(fin.dat$snp))/sd(fin.dat$snp)
+  			fin.dat <- fin.dat[fin.dat$FID%in%fin.dat$FID[fin.dat$ind==1 & fin.dat$Y==1],,drop=F]
+  			PB <- 'ind'
+  
+  			model <- Y~std_snp-1
+  			out <- paste0("~/paper/heritability/ML_ver2/variousFam/prev_",prev,"_h2_",h2,"/Esth2_",totalfam,"_newasc.txt")
+  			write.table(data.frame('obs','beta_std_snp','h2','n_iteration'),out,col.names=F,row.names=F,quote=F)
+  			Esth2 <- sapply(301:600,getEsth2.asc,fin.dat=fin.dat,init_beta=0.1,init_h2=h2,totalfam=totalfam,assumed_prev=prev,model=model,n.cores=n.cores,out=out,seed=T,PB=PB)
+  		}
+  	}
   }
 }
 
-for(h2 in c(0.05,0.2,0.4)) est.ft(0.05,h2,500,32)
+## n10, 24 cores, p 0.1 h2 0.05
+est.ft(0.1,0.05,500,24)
+
+## n11, 24 cores, p 0.2 h2 0.05
+est.ft(0.2,0.05,500,24)
--- LTM_heritability/SW_comparison/SW_comparison_code.R
+++ LTM_heritability/SW_comparison/SW_comparison_code.R
@@ -49,11 +49,30 @@ for(prev in c(0.05,0.1,0.2)){
 
       model <- Y~std_snp-1
       out <- paste0("~/paper/heritability/ML_ver2/variousFam/otherSW/1.GCTA/prev_",prev,"_h2_",h2,"/Esth2_",totalfam,"_asc.txt")
-      write.table(data.frame('obs','beta_std_snp','h2','n_iteration'),out,col.names=F,row.names=F,quote=F)
+      write.table(data.frame('Obs','h2','SE_h2'),out,col.names=F,row.names=F,quote=F)
       working_dir <- paste0("~/paper/heritability/ML_ver2/variousFam/otherSW/1.GCTA/prev_",prev,"_h2_",h2)
       Esth2 <- sapply(1:300,getEsth2.GCTA,fin.dat=fin.dat,totalfam=totalfam,prev=prev,res_var='Y',exp_var='std_snp',out=out,working_dir=working_dir,seed=T)
     }
   }
 }
+Obs h2 SE_h2
 
+#### Mis-specified prevalence
+source('~/paper/heritability/ML_ver2/LTM_heritability_ML_ver2.R')
+for(prev in c(0.05,0.1,0.2)){
+  for(h2 in c(0.05,0.2,0.4)){
+    for(totalfam in c(500)){
+	  assumed_prev <- prev+0.1
+      print(paste0('prevalence:',prev,', assumed prevalence:',assumed_prev,', heritability:',h2,', number of families:',totalfam))
+      library(kinship2)
+      setwd("/home2/wjkim/paper/heritability/ML_ver2/variousFam/")
+      fin.dat <- read.table(paste0("prev_",prev,"_h2_",h2,"/dataset.txt"),head=T,stringsAsFactor=F)
+      fin.dat$std_snp <- (fin.dat$snp-mean(fin.dat$snp))/sd(fin.dat$snp)
+      out <- paste0("/home2/wjkim/paper/heritability/ML_ver2/variousFam/otherSW/1.GCTA/prev_",prev,"_h2_",h2,"/Esth2_",totalfam,"_misspecPrev.txt")
+      write.table(data.frame('Obs','h2','SE_h2'),out,col.names=F,row.names=F,quote=F)
+      working_dir <- paste0("/home2/wjkim/paper/heritability/ML_ver2/variousFam/otherSW/1.GCTA/prev_",prev,"_h2_",h2)
+      Esth2 <- sapply(1:300,getEsth2.GCTA,fin.dat=fin.dat,totalfam=totalfam,prev=assumed_prev,res_var='Y',exp_var='std_snp',out=out,working_dir=working_dir,seed=T)
+    }
+  }
+}
 
