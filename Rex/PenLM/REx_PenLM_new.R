e175232458c0c8c4a427f958b31201a5d3e2e0e4
Wonji Kim <dnjswlzz11@gmail.com>
2019-02-20 00:06:03 -0500

    [Rex] 벌점화회귀모형

--- Rex/PenLM/REx_PenLM_new.R
+++ Rex/PenLM/REx_PenLM_new.R
@@ -743,24 +743,120 @@ Digits <- function(x) {
   return(x1)
 }
 
-inst.pkg <- function(pkg, repos="http://healthstat.snu.ac.kr/CRAN") {
-  if (!suppressWarnings(require(pkg, quietly=TRUE, character.only=TRUE))) {
-    ret <- try(local.install.packages(pkg, repos=repos), T)
-    if (class(ret) == 'try-error') 2
-    else 1
-  }
-  0
-}
+###
+### glmnet wrapper function provides formula
+###
 
-# Package List
-indiv.pkg.info <- function(pkg.info) {
-  funs <- strsplit(pkg.info[[2]],",\\s*")[[1]]
-  paste0("<li> ",pkg.info[[1]]," : ",
-         paste0(paste0(paste0("function ",
-                              paste(sapply(funs,function(v)paste0("'<a href=\"https://www.rdocumentation.org/packages/", pkg.info[[3]], "/topics/", v, "\" target='_new'>", v, "</a>'")),collapse=", "),
-                              " of R package '"),"<a href=\"https://www.rdocumentation.org/packages/", pkg.info[[3]], "\" target='_new'>", pkg.info[[3]],"</a>'"),collapse=", "))
+glmnet_wrapper <- function(formula, data,
+                           family = c("gaussian", "binomial"), 
+                           alpha = 1, nlambda = 100, lambda = NULL, standardize = TRUE, intercept = TRUE, ...) 
+{
+  cl <- match.call()
+  mf <- match.call(expand.dots = FALSE)
+  m <- match(c("formula", "data"), names(mf), 0L)
+  mf <- mf[c(1L, m)]
+  mf$drop.unused.levels <- TRUE
+  mf[[1L]] <- quote(stats::model.frame)
+  mf <- eval(mf, parent.frame())
+  
+  mt <- attr(mf, "terms")
+  y <- model.response(mf, "numeric") #w <- as.vector(model.weights(mf))
+  
+  ###
+  ###
+  #print(is.empty.model(mt))
+  if(is.empty.model(mt)){
+    x <- NULL
+    z <- list(coefficients = if (is.matrix(y))  matrix(, 0, 3) else numeric(), 
+              residuals = y, fitted.values = 0*y, weights = w, rank = 0L, 
+              df.residual = if (!is.null(w)) sum(w != 0) else if (is.matrix(y)) nrow(y) else length(y))
+  }else {
+    x <- model.matrix(mt, mf)
+    #R2HTML::HTML("HERE", file="./test.html")
+    z <- glmnet::glmnet(x, y, family = family, offset = NULL, 
+                        alpha = alpha, nlambda = nlambda, lambda = lambda, 
+                        standardize = standardize, intercept = intercept, ...)
+    
+    # model selection via validation data set
+    #    if(!is.null(validid)){
+    #      pred <- predict.glmnet(z, x[validid,], s=z$lambda, type="response")
+    #      R2HTML::HTML(c(dim(pred), length(z$lambda)), file="./test.html")
+    #      res <- pred-matrix(rep(y[validid], times=length(z$lambda)), nrow=length(validid))
+    #      mse <- apply(res^2, 2, mean)
+    #      R2HTML::HTML(mse, file="./test.html")
+    
+    #      z <- glmnet::glmnet(x, y, family = family, weights = w, offset = NULL, 
+    #             alpha = alpha, lambda = z$lambda[which.min(mse)], standardize = standardize, 
+    #             intercept = intercept, ...)
+    #    }
+  }
+  z$mt <- mt
+  z$mf <- mf
+  z$fx <- predict.glmnet(z, x, s=z$lambda, type="response")
+  z$digits = max(3, getOption("digits") - 3)
+  z
 }
-used.pkg <- function(pkg.list) paste(c(sapply(pkg.list,indiv.pkg.info),"<li> All results other than those mentioned above were written with basic functions of R."),collapse=" <br> ")
+predict_glmnet_wrapper <- function(object, newx, s=NULL, type = c("link", "response", 
+                                                                  "coefficients", "nonzero", "class"))
+{
+  if(class(newx) !="matrix")
+    newx <- as.matrix(newx)
+  
+  predict.glmnet(object, newx, s, type)
+}
+predict_cv.glmnet_wrapper <- function(object, newx, s = c("lambda.1se", "lambda.min"), ...) 
+{
+  if(class(newx) !="matrix")
+    newx <- as.matrix(newx)
+  
+  predict.cv.glmnet(object, newx, s="lambda.min", ...) 
+}
+###
+### cv.glmnet wrapper function provides formula
+###
+cv_glmnet_wrapper <- function(formula, data, 
+                              weights, offset=NULL, lambda = NULL, 
+                              type.measure = c("mse", "mae"), nfolds = 10, 
+                              family = c("gaussian", "binomial"), 
+                              alpha = 1, nlambda = 100, standardize = TRUE, intercept = TRUE, opt.lambda="lambda.min")
+{
+  cl <- match.call()
+  mf <- match.call(expand.dots = FALSE)
+  m <- match(c("formula", "data"), names(mf), 0L)
+  mf <- mf[c(1L, m)]
+  mf$drop.unused.levels <- TRUE
+  mf[[1L]] <- quote(stats::model.frame)
+  mf <- eval(mf, parent.frame())
+  
+  mt <- attr(mf, "terms")
+  y <- model.response(mf, "numeric")
+  
+  ###
+  ###
+  ###
+  if(is.empty.model(mt)){
+    x <- NULL
+    z <- list(coefficients = if (is.matrix(y))  matrix(, 0, 3) else numeric(), 
+              residuals = y, fitted.values = 0*y, weights = w, rank = 0L, 
+              df.residual = if (!is.null(w)) sum(w != 0) else if (is.matrix(y)) nrow(y) else length(y))
+    
+    if (!is.null(offset)) {
+      z$fitted.values <- offset
+      z$residuals <- y - offset
+    }
+  }else {
+    x <- model.matrix(mt, mf)
+    z <- glmnet::cv.glmnet(x, y, lambda = lambda, 
+                           type.measure = type.measure, nfolds = nfolds, 
+                           family = "gaussian", alpha = alpha, nlambda = nlambda, 
+                           standardize = standardize, intercept = intercept) 
+  }  
+  z$mt <- mt
+  z$mf <- mf
+  z$fx <- predict.cv.glmnet(z, x, s=opt.lambda, type="response")
+  z$digits = max(3, getOption("digits") - 3)
+  z
+}
 
 # Check integer
 check.integer <- function(x) x == round(x)
--- Rex/PenLM/exec_new.R
+++ Rex/PenLM/exec_new.R
@@ -1,8 +1,9 @@
 #setwd('C://Users//rewki//Documents//WJ_codes//Rex//PenLM')
 setwd("~/WJ_codes/Rex/PenLM/")
-source('./REx_glmnet.R', encoding = 'UTF-8')
+#source('./REx_glmnet.R', encoding = 'UTF-8')
 source('./REx_PenLM_new.R')
-source('./glmnet.wrapper.1.0.R')
+source('../확인된오류/prior_ft.txt')
+
 set.seed(1)
 n <- 100; p <- 6
 x <- matrix(rnorm(n*p), n, p)
@@ -26,6 +27,16 @@ ex0 <- REx_PenLM(temp,dep_var='y',
                  AccMS='MSE',Part.method='percent', train.perc=70, Profile=TRUE,
                  Best_model_print=TRUE, ss="III")
 
+ex0 <- REx_PenLM(temp,dep_var='y',
+                 indep_cat_var=NULL,
+                 indep_numeric_var=xname,vars=xname,noint=T,
+                 Penalty='Lasso', 
+                 TuningPara='Grid',Grid.Num=100,
+                 Cross.method='LOOCV',
+                 AccMS='MSE',Part.method='percent', train.perc=70, Profile=TRUE,
+                 Best_model_print=TRUE, ss="III")
+
+
 ex1 <- REx_PenLM(temp,dep_var='y',
                  indep_cat_var=NULL,
                  indep_numeric_var=xname,vars=xname,
@@ -37,7 +48,7 @@ ex1 <- REx_PenLM(temp,dep_var='y',
 
 ex2 <- REx_PenLM(temp,dep_var='y',
                  indep_cat_var=NULL,
-                 indep_numeric_var=c('x3','x4'),vars=c('x3','x4'),noint=F,
+                 indep_numeric_var=c('x3','x4'),vars=c('x3','x4'),noint=T,
                  Penalty='Lasso', 
                  TuningPara='Grid',Grid.Num=100,
                  Cross.method='LOOCV',
@@ -45,45 +56,12 @@ ex2 <- REx_PenLM(temp,dep_var='y',
                  Best_model_print=TRUE, ss="III")
 
 
-#REx_PenLM <- function(dataset,dep_var,indep_cat_var=NULL,indep_numeric_var=NULL,
-#                      vars=NULL,intercept=FALSE,standardize=TRUE,
-#                      Penalty=c('Ridge','Lasso','EN'),alpha=0.5,
-#                      TuningPara=c('Grid','Custom'),Grid.Num=100,Custom.List=NULL,
-#                      Cross.method=c('KFOLD','LOOCV'),k=10,AccMS=c('MSE','MAE'),
-#                      Part.method=c('all','percent','variable'),train.perc=70,Part.var=NULL,
-#                      Profile=FALSE,Best_model_print=TRUE,CI=TRUE,confint.level=0.95,
-#                      VIF=FALSE,ANOVA=TRUE,ss=c('I','II','III'),GOF=FALSE,Plot=FALSE,
-#                      Best_model_save=FALSE,Predict_train=FALSE,Predict_CI_train=FALSE,
-#                      Predict_PI_train=FALSE,confint.level_train=0.95,Resid=FALSE,
-#                      stdResid=FALSE,studResid=FALSE,cook_distance=FALSE,hat_value=FALSE,
-#                      Predict_test=FALSE,Predict_CI_test=FALSE,Predict_PI_test=FALSE,confint.level_test=0.95,Part_index=FALSE){
-  
-#fit <- glmnet_wrapper(y~., data=temp, family = "gaussian", 
-#  alpha = 1, nlambda = 100, lambda = NULL, standardize = TRUE, intercept = TRUE) 
-
-#fit2 <- cv_glmnet_wrapper(y~., data=temp, type.measure = "mse", nfolds = 10, 
-#  family = "gaussian", alpha = 1, nlambda = 100, standardize = TRUE, intercept = TRUE, opt.lambda="lambda.min")
-
-#predict.cv.glmnet(fit2, newx=, s = c("lambda.1se", "lambda.min"), ...) 
-
-#ex1 <- REx_glmnet(temp, res_var='y', quan_var=xname, 
-#         vars=xname, family = "gaussian", standardize = TRUE, intercept=TRUE, 
-#         alpha=1, Valid.method = "Noselection", #c("Partition", "Cross", "Noselection"), 
-#         Part.method = "percent", #c("percent", "variable"),
-#         train.perc = 70, Part.var = NULL,
-#         Cross.method= "LOOCV", #c("LOOCV", "KFOLD"),
-#         nfolds = 10,
-#         TuningPara = "Grid", 
-#         Grid.num = 100, Custom.List = NULL,
-#         type.measure="mse", opt.lambda="lambda.min", 
-#         solutionpath_plot=TRUE, Predict=FALSE, Coef=FALSE)
-
-#ex2 <- REx_glmnet(temp, res_var='y', quan_var=xname,          vars=xname, family = "gaussian", standardize = TRUE, intercept=TRUE,          alpha=1/2, Valid.method = "Cross", #"Partition", #c("Partition", "Cross", "Noselection"), 
-#         Part.method = "percent", #c("percent", "variable"),         train.perc = 80, Part.var = NULL,
-#         Cross.method= "KFOLD", #c("LOOCV", "KFOLD"),         nfolds = 5,
-#         TuningPara = "Grid",          Grid.num = 100, Custom.List = c(3,2.5,2,1,1.5,0),
-#         type.measure="mse", opt.lambda="lambda.min",          solutionpath_plot=T, Predict=T, Coef=T)
-
-
-#aa <- glmnet.wrapper(y~x1+x2, data=temp, family = "gaussian", alpha=1, nlambda = 100, lambda = NULL, standardize = TRUE, intercept = TRUE) 
-#write.table(ex1$html, "a.html")
+dataset <- temp;
+dep_var='y';
+indep_cat_var=NULL;
+indep_numeric_var=xname;vars=xname;noint=F;
+Penalty='Lasso'; 
+TuningPara='Grid';Grid.Num=100;
+Cross.method='LOOCV';
+AccMS='MSE';Part.method='percent'; train.perc=70; Profile=TRUE;
+Best_model_print=TRUE; ss="III"
